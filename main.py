7f9fb3eb867569ab5334128120c1240642c6cdd6
Dominic Harness <domharness@gmail.com>
2023-07-09 16:25:31 -0600

    Update main.py
    update to include content manager presets

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
+import time
 
-# Set the paths and arguments to the scripts you need.
 # Path to first script and its arguments
 script1_path = 'joytokey.py'
 script1_arguments = ['Assetto Corsa - Drift']
@@ -9,9 +9,9 @@ script1_arguments = ['Assetto Corsa - Drift']
 script2_path = 'fanatec_control_panel_preset.py'
 script2_arguments = ['1']
 
-
-
-
+# Path to third script and its arguments
+script3_path = 'cm_presets.py'
+script3_arguments = ['--ac_audio_preset', 'TV-V1', '--ac_video_preset', 'HQ-HDR', '--cm_preset', 'HQ-DriftV3']
 
 def call_script(script_path, arguments):
     command = ['python', script_path] + arguments
@@ -23,6 +23,7 @@ def call_script(script_path, arguments):
         print(f"Return code: {e.returncode}")
         print(f"Error output: {e.output.decode()}")
 
+
 # Call first script
 print(f"Calling script: {script1_path}")
 call_script(script1_path, script1_arguments)
@@ -30,3 +31,7 @@ call_script(script1_path, script1_arguments)
 # Call second script
 print(f"Calling script: {script2_path}")
 call_script(script2_path, script2_arguments)
+
+# Call third script
+print(f"Calling script: {script3_path}")
+call_script(script3_path, script3_arguments)
