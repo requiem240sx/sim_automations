2244bb56dc3a69c03f4c82cbff4c9c1da3586154
Dominic Harness <domharness@gmail.com>
2023-07-08 19:18:45 -0600

    Update joytokey.py
    adjustments for main script to call it

--- joytokey.py
+++ joytokey.py
@@ -1,41 +1,41 @@
 import subprocess
 import time
 import os
+import argparse
 from pywinauto import Application
 
 # Define constants
 OPEN_JOYTOKEY_PATH = r"C:\Program Files (x86)\JoyToKey\JoyToKey.exe"
 SLEEP_DURATION = 1  # Duration to wait for an application to load
-JOYTOKEY_LIST_ITEM = "Assetto Corsa - Drift"
 
 def launch_application(app_path):
     """Launches an application at the specified path."""
     try:
         subprocess.Popen([app_path])
-        print(f"Successfully launched {app_path}")
+        print(f"JoyToKey - Successfully launched {app_path}")
     except Exception as e:
-        print(f"Failed to launch {app_path}: {e}")
+        print(f"JoyToKey - Failed to launch {app_path}: {e}")
 
 def connect_to_application(app_path):
     """Connects to an application and returns its main window."""
     try:
         app = Application(backend="uia").connect(path=app_path)
         main_window = app.window()
-        print(f"Successfully connected to {app_path}")
+        print(f"JoyToKey - Successfully connected to {app_path}")
         return main_window
     except Exception as e:
-        print(f"Failed to connect to {app_path}: {e}")
+        print(f"JoyToKey - Failed to connect to {app_path}: {e}")
 
 def focus_on_window(window):
     """Sets focus on the specified window."""
     try:
         window.set_focus()
-        print("Window focus set")
+        print("JoyToKey - Window focus set")
     except Exception as e:
-        print(f"Failed to set focus: {e}")
+        print(f"JoyToKey - Failed to set focus: {e}")
 
-def main():
-    # Launch JoyToKey 
+def main(joytokey_list_item):
+    # Launch JoyToKey
     launch_application(OPEN_JOYTOKEY_PATH)
     time.sleep(SLEEP_DURATION)  # Wait for the application to load
 
@@ -47,12 +47,20 @@ def main():
     focus_on_window(main_window)
 
     # Get the parent list control using AutomationId
-    list_control = main_window.child_window(auto_id="395730", control_type="List")
+    list_control = main_window.child_window(class_name="TListBox")
+    
 
     # Get the list item and click on it
-    list_item = list_control.get_item(JOYTOKEY_LIST_ITEM)
+    list_item = list_control.get_item(joytokey_list_item)
     list_item.click_input()
-    print("JoyToKey Profile: " + JOYTOKEY_LIST_ITEM)
+    print("JoyToKey - Profile Selected: " + joytokey_list_item)
+
+    # Minimize the window
+    main_window.minimize()
+    print("Window minimized")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("joytokey_list_item", help="The JoyToKey list item to select")
+    args = parser.parse_args()
+    main(args.joytokey_list_item)
